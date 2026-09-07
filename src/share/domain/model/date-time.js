7f99a17cdf0a02c63208ec3fef56ff6de3ce6245
Andrea <aikhor0907@gmail.com>
2026-09-07 17:53:22 -0500

    feat(shared):add date-time.js

---
+++ src/share/domain/model/date-time.js
@@ -0,0 +1,30 @@
+import {ValidationError} from "./errors.js";
+
+export class DateTime {
+    #date;
+    constructor(date=new Date()) {
+        const parsedDate =date instanceof Date ? new Date(date) : date;
+        if (isNaN(parsedDate.getTime())){
+            throw new ValidationError(`Invalid date: ${date}`);
+        }
+        this.#date = new Date(parsedDate.getTime()); {
+            Object.freeze(this);
+        }
+
+    }
+    get date() {
+        return new Date(this.#date.getTime());
+    }
+
+    toISOString() {
+        return this.#date.toISOString();
+    }
+
+    toString() {
+        return this.#date.toString();
+    }
+
+    equals(other) {
+        return other instanceof DateTime && thisthis.#date.getTime() === other.getTime();
+    }
+}
