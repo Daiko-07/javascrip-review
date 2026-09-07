fb54399eb09d26ab9187b8fa44039af292602ad6
Andrea <aikhor0907@gmail.com>
2026-09-07 18:20:16 -0500

    feat end

--- src/share/domain/model/purchase-order-id.js
+++ src/share/domain/model/purchase-order-id.js
@@ -1,2 +1,30 @@
 import {generateUuid, validateUuid} from "./uuid.js";
-import {validateError} from "./error.js";
+import {ValidationError} from "./errors.js";
+
+export class PurchaseOrderId {
+    #value;
+
+    constructor(value) {
+        if (!validateUuid(value)) {
+            throw new ValidationError(`Invalid PurchaseOrderID: ${value}. Must be a valid UUID.`);
+        }
+        this.#value = value;
+        Object.freeze(this);
+    }
+
+    get value() {
+        return this.#value;
+    }
+
+    toString() {
+        return this.#value;
+    }
+
+    equals(other) {
+        return other instanceof PurchaseOrderId && this.#value === other.#value;
+    }
+
+    static generate() {
+        return new PurchaseOrderId(generateUuid());
+    }
+}
